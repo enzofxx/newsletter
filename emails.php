530f9a2f4452889eba4cf587f97db4f821e83eb7
enzofxx <35643620+enzofxx@users.noreply.github.com>
2022-05-06 02:20:03 +0300

    added validations, some styles

--- emails.php
+++ emails.php
@@ -1,11 +1,34 @@
 <?php
+
 class Email {
     public static function addEmail() {
+        // getting value from JS
         $userEmail = $_POST['useremail'];
-        $emails = json_decode(file_get_contents("emails.json"));
-        array_push($emails, $userEmail);
-        $emails = json_encode($emails);
-        file_put_contents('emails.json', $emails);
+        // checking if file exist
+        try {
+            $emails = json_decode(file_get_contents("emails.json")); 
+        } catch (Exception $e){}  
+        // few validations
+        if (!strpos($userEmail, '@') || !strpos($userEmail, '.')) {
+            exit('<p class="red">Įvestas e. pašto adresas turi turėti „@“ ir „.“ simbolius.</p>');
+        }
+
+        if (in_array($userEmail, $emails)){
+            exit('<p class="red">Įvestas e. pašto adresas jau yra sąraše.</p>');
+        }
+
+        if (strpos($userEmail, '@') + 1 === strpos($userEmail, '.')) {
+            exit('<p class="red">Taškas negali eiti po @ simbolio.</p>');
+        }
+        // if everything is okay - pushing to JSON
+        if (file_exists('emails.json')) {
+            array_push($emails, $userEmail);
+            $emails = json_encode($emails);
+            file_put_contents('emails.json', $emails);
+            exit('<p class="green">Jūs sėkmingai užsiprenumeravote naujienlaiškį.</p>');
+        } else {
+            exit('<p class="red">Atsiprašome, įvyko klaida.</p>');
+        }
     }
 }
 
--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-let email = document.getElementById('app');
-email.innerHTML = `<form>
-<input id="userEmail" type="email">
-<div id="submit" class="button">Prenumeruoti</div>
-</form>`;
+// creating starting form
+function form () {
+  let email = document.getElementById('app');
+  email.innerHTML = `<form>
+  <input id="userEmail" type="email">
+  <div id="submit" class="button">Prenumeruoti</div>
+  </form>`;
+}
+form();
 
 let send = document.getElementById('submit');
 
-
 send.addEventListener("click", function(){
     let userEmail = document.getElementById('userEmail').value;
-    
+    // fecthing data to php
     fetch("http://localhost/newsletter/emails.php", {
       method: "POST",
       headers: {
